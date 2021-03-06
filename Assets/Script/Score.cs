c2a801c1552f8d05de4bb105bc2e79a515cbd705
Hussein Isron <isronid@gmail.com>
2021-03-06 22:29:42 +0700

    asset
    penambahan asset

---
+++ Assets/Script/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public Text scoreText;
+    public float scoreAmount;
+    public float pointIncreasePerSecond;
+
+    // Use this for initialization
+    void Start () {
+        scoreAmount = 0f;
+        pointIncreasePerSecond = 1f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = (int)scoreAmount + " Score";
+        scoreAmount += pointIncreasePerSecond * Time.deltaTime;
+	}
+}
---
+++ Assets/Script/karutController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class karutController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    private float moveInput;
+    public float jumpForce;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+
+    private Animator anim;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+        if (moveInput == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+        }
+        if(moveInput > 0){
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }else if(moveInput < 0){
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.SetTrigger("takeOff");
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        if (isGrounded == true)
+        {
+            anim.SetBool("isJumping", false);
+        }
+        else
+        {
+            anim.SetBool("isJumping", true);
+        }
+        if(Input.GetKey(KeyCode.UpArrow) && isJumping == true){
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+                
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            isJumping = false;
+        }
+    }
+}
